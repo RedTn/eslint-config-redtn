d0ed82cee0d0e2cfa58c7cea14edfa781dcac61f
RedTn <redmond.tran@gmail.com>
2018-07-21 00:54:39 -0700

    fix: line style incorrect syntax

--- packages/eslint-config-redtn/rules/base.js
+++ packages/eslint-config-redtn/rules/base.js
@@ -1,31 +1,34 @@
 module.exports = {
   rules: {
-      'linebreak-style': 'unix',
-      'indent': [
-          'error',
-          4,
-          {
-              'SwitchCase': 1
-          }
-      ],
-      'eqeqeq': [
-          'error',
-          'smart'
-      ],
-      'curly': [
-          'error'
-      ],
-      'quotes': [
-          'error',
-          'single'
-      ],
-      'semi': [
-          'error',
-          'always'
-      ],
-      'requireTrailingComma': 0,
-      'no-console': 1,
-      'no-case-declarations': 0,
-      'disallowQuotedKeysInObjects': 0
-  }
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    'indent': [
+        'error',
+        4,
+        {
+            'SwitchCase': 1
+        }
+    ],
+    'eqeqeq': [
+        'error',
+        'smart'
+    ],
+    'curly': [
+        'error'
+    ],
+    'quotes': [
+        'error',
+        'single'
+    ],
+    'semi': [
+        'error',
+        'always'
+    ],
+    'requireTrailingComma': 0,
+    'no-console': 1,
+    'no-case-declarations': 0,
+    'disallowQuotedKeysInObjects': 0
+}
 };
